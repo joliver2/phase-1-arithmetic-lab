20ade00e6bb33f35f564f02569caa04276702877
joliver427@icloud.com <joliver427@icloud.com>
2022-12-13 12:47:50 -0700

    commit tree 6f6ec5cc2bce8f0be84b285b36146147295d62b2

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 2
+let num2 = 31
+let num3 = 9
+let num4 = 5
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num3 % num4;
+let max = Math.max(1, 7, 20, 3, 9);
